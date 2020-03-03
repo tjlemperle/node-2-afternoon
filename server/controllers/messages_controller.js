c386b737a0e04824ef70fb4dc673839c8d4472a1
tjlemperle <tjlemperle@gmail.com>
2020-03-03 14:42:18 -0700

    working

--- server/controllers/messages_controller.js
+++ server/controllers/messages_controller.js
@@ -18,18 +18,18 @@ module.exports = {
 
     //kinda confused on update. work on this
     update: (req, res) => {
-    const { text } = req.body;
-    const updateID = req.params.id;
-    const messageIndex = messages.findIndex(message => message.id == updateID);
-    let message = messages[messageIndex];
-
-    messages[messageIndex] = {
-      id: message.id,
-      text: text || message.text,
-      time: message.time
-    };
-
-    res.status(200).send(messages);
+        const { text } = req.body;
+        const updateID = req.params.id;
+        const messageIndex = messages.findIndex(message => message.id == updateID);
+        let message = messages[messageIndex];
+    
+        messages[messageIndex] = {
+          id: message.id,
+          text: text || message.text,
+          time: message.time
+        };
+    
+        res.status(200).send(messages);
     },
     delete: (req, res) => {
         let {id} = req.body
